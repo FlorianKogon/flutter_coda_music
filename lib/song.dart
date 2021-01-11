ebd2a3115e44e0ca0fc209b107c9453ef8d23b69
Florian Kogon <F.Kogon@gmail.com>
2021-01-11 14:31:04 +0100

    class-song-created

---
+++ lib/song.dart
@@ -0,0 +1,14 @@
+class Song {
+
+  String title;
+  String author;
+  String imagePath;
+  String urlSong;
+
+  Song(String title, String author, String imagePath, String urlSong) {
+    this.title = title;
+    this.author = author;
+    this.imagePath = imagePath;
+    this.urlSong = urlSong;
+  }
+}
